9ef53639f3b392856d03767a9a53339f97dc659b
BrianWalczak <72092838+BrianWalczak@users.noreply.github.com>
2023-03-13 21:08:11 -0500

    Add hidden links

--- static/index.js
+++ static/index.js
@@ -10,8 +10,14 @@ form.addEventListener('submit', async event => {
         if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
-
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+        document.body.innerHTML = ''; //Reset documents contents
+        const iframe = document.createElement("iframe");
+        iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+        iframe.style.position = "fixed";
+        iframe.style.top = iframe.style.bottom = iframe.style.left = iframe.style.right = 0;
+        iframe.style.border = iframe.style.outline = "none";
+        iframe.style.width = iframe.style.height = "100%";
+        document.body.appendChild(iframe);
     });
 });
 
