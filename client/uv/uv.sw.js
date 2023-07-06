96e950dfd2f4db71b852dbcd21de9ba2ff341768
Brian Walczak <72092838+BrianWalczak@users.noreply.github.com>
2023-07-05 21:24:03 -0500

    Update uv.sw.js

--- client/uv/uv.sw.js
+++ client/uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
+importScripts('/ph/uv.bundle.js');
+importScripts('/ph/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
