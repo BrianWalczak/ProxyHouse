e596e89a0a309b067169ffa2fee28ec8a7854a86
Brian <72092838+BrianWalczak@users.noreply.github.com>
2024-02-13 17:41:53 -0600

    Update and rename sw.js to main.js

---
+++ client/main.js
@@ -0,0 +1,20 @@
+function isUrl(val = '') {
+    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+    return false;
+};
+
+async function register() {
+  if (!navigator.serviceWorker) throw new Error("Your browser doesn't support service workers.");
+
+  await navigator.serviceWorker.register("/uv/sw.js", {
+    scope: __uv$config.prefix,
+  });
+}
+
+async function search() {
+  let url = input.value.trim();
+  if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+  else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+  return url;
+}
