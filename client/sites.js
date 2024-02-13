0c850843696c33c505e70977d4438a997a72c5bd
Brian <72092838+BrianWalczak@users.noreply.github.com>
2024-02-13 17:58:00 -0600

    Update sites.js

--- client/sites.js
+++ client/sites.js
@@ -1,37 +1,34 @@
 function launch(link) {
-	window.navigator.serviceWorker.register('./sw.js', {
-		scope: __uv$config.prefix
-	}).then(() => {
-		let url = link.trim();
+  const proxiedUrl = await search(link);
 
-		if(getSetting("cloak_mode") == false) {
-			document.body.innerHTML = ''; //Reset documents contents
-    	const iframe = document.createElement("iframe");
-    	iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
-    	iframe.style.position = "fixed";
-    	iframe.style.top = iframe.style.bottom = iframe.style.left = iframe.style.right = 0;
-    	iframe.style.border = iframe.style.outline = "none";
-    	iframe.style.width = iframe.style.height = "100%";
-    	document.body.appendChild(iframe);
-		}else{
-			win = window.open();
-			win.document.body.style.margin = '0';
-      win.document.body.style.height = '100vh';
-      var iframe = win.document.createElement('iframe');
-      iframe.style.border = 'none';
-      iframe.style.width = '100%';
-      iframe.style.height = '100%';
-      iframe.style.margin = '0';
-      iframe.src = location.protocol + '//' + location.host + __uv$config.prefix + __uv$config.encodeUrl(url);
-      win.document.body.appendChild(iframe);
+  if(getSetting("cloak_mode") == false) {
+    document.body.innerHTML = '<head><link id="favicon" rel="shortcut icon" type="image/png" /></head>'; //Reset documents contents but add favicon item
+    
+    const iframe = document.createElement("iframe");
+    iframe.src = proxiedUrl;
+    iframe.style.position = "fixed";
+    iframe.style.top = iframe.style.bottom = iframe.style.left = iframe.style.right = 0;
+    iframe.style.border = iframe.style.outline = "none";
+    iframe.style.width = iframe.style.height = "100%";
+    document.body.appendChild(iframe);
+  } else {
+    win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
 
-			//Checking if the disguiser is enabled
-			if(getSetting('page_disguiser') == true) {
-				win.document.title = "Classes";
-				
-				win.document.getElementsByTagName("head")[0].innerHTML += '<link id="favicon" rel="shortcut icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png" />'
-			}
-			
-		}
-	});
+    var iframe = win.document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.margin = '0';
+    iframe.src = proxiedUrl;
+    win.document.body.appendChild(iframe);
+
+    // Checking if the disguiser is enabled
+    if(getSetting('page_disguiser') == true) {
+      win.document.title = "Classes";
+      
+      win.document.getElementsByTagName("head")[0].innerHTML += '<link id="favicon" rel="shortcut icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png" />'
+    }
+  }
 }
