4a349d5b1b0329629cdba22e1dc84afc5e1d18a7
Brian <72092838+BrianWalczak@users.noreply.github.com>
2024-02-13 10:35:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const createServer = require('@tomphttp/bare-server-node');
-const http = require('http');
-const nodeStatic = require('node-static');
+import { createBareServer } from "@tomphttp/bare-server-node";
+import express from "express";
+import { createServer } from "node:http";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { join } from "node:path";
+import { hostname } from "node:os";
 
+const bare = createBareServer("/bare/");
+const app = express();
 
-const bare =  createServer('/bare/');
-const serve = new nodeStatic.Server('client/');
+app.use("/uv/", express.static(uvPath));
+const server = createServer();
 
-const server = http.createServer();
-
-server.on('request', (req, res) => {
+server.on("request", (req, res) => {
     if (bare.shouldRoute(req)) {
 		bare.routeRequest(req, res);
 	} else {
-		serve.serve(req, res);
+		app(req, res);
 	}
 });
 
 server.on('upgrade', (req, socket, head) => {
-	if (bare.shouldRoute(req, socket, head)) {
+	if (bare.shouldRoute(req)) {
 		bare.routeUpgrade(req, socket, head);
 	}else{
 		socket.end();
