45906cfdfdaa99d6c19e32430d1bbba0a348de14
Brian <72092838+BrianWalczak@users.noreply.github.com>
2024-02-13 17:48:56 -0600

    Update main.js

--- client/main.js
+++ client/main.js
@@ -23,5 +23,5 @@ async function search(query) {
   if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
   else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
   
-  return url;
+  return __uv$config.prefix + __uv$config.encodeUrl(url);
 }
