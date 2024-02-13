685685a4cc9c93ad079218571cd93f079303c698
Brian <72092838+BrianWalczak@users.noreply.github.com>
2024-02-13 10:38:34 -0600

    Update sw.js

--- client/sw.js
+++ client/sw.js
@@ -1,9 +1,31 @@
-importScripts('./uv/uv.sw.js');
+"use strict";
+/**
+ * Distributed with Ultraviolet and compatible with most configurations.
+ */
+const stockSW = "/uv/sw.js";
 
-const sw = new UVServiceWorker();
+/**
+ * List of hostnames that are allowed to run serviceworkers on http://
+ */
+const swAllowedHostnames = ["localhost", "127.0.0.1"];
 
-self.addEventListener('fetch', event =>
-    event.respondWith(
-        sw.fetch(event)
+/**
+ * Global util
+ * Used in 404.html and index.html
+ */
+async function registerSW() {
+  if (!navigator.serviceWorker) {
+    if (
+      location.protocol !== "https:" &&
+      !swAllowedHostnames.includes(location.hostname)
     )
-);
+      throw new Error("Service workers cannot be registered without https.");
+
+    throw new Error("Your browser doesn't support service workers.");
+  }
+
+  // Ultraviolet has a stock `sw.js` script.
+  await navigator.serviceWorker.register(stockSW, {
+    scope: __uv$config.prefix,
+  });
+}
