5c7dc768191bd7da11489c5a412ec625dabf8d45
Brian <72092838+BrianWalczak@users.noreply.github.com>
2024-02-13 10:40:20 -0600

    Update web.js

--- client/web.js
+++ client/web.js
@@ -3,9 +3,7 @@ const input = document.querySelector('input');
 
 form.addEventListener('submit', async event => {
     event.preventDefault();
-    window.navigator.serviceWorker.register('./sw.js', {
-        scope: __uv$config.prefix
-    }).then(() => {
+    registerSW().then(() => {
         let url = input.value.trim();
         if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
