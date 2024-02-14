03b6f27e5c89bf4a7480038856c66cf1d8a7d5ac
Brian <72092838+BrianWalczak@users.noreply.github.com>
2024-02-13 18:13:00 -0600

    Update checkSettings.js

--- client/checkSettings.js
+++ client/checkSettings.js
@@ -34,3 +34,15 @@ if(getSetting('page_disguiser') == true) {
 	
 	document.getElementById("favicon").href = "https://ssl.gstatic.com/classroom/favicon.png"
 }
+
+if(getSetting('prevent_close') == true) {
+	window.addEventListener('beforeunload', function(e) {
+    e.preventDefault();
+    
+    let confirm = "Heads up! An attempt has been made to close this page.";
+    
+    e.returnValue = confirm;
+    (e || window.event).returnValue = confirm;
+    return confirm;
+});
+}
